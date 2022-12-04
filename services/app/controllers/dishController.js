4b7114d9bd9a16ac36eaaa2c17d3d1959b4b0e33
Ibrahimad98 <ibrahim.ad90@gmail.com>
2022-12-05 03:57:26 +0700

    fix get dishes by category

--- services/app/controllers/dishController.js
+++ services/app/controllers/dishController.js
@@ -2,13 +2,31 @@ const { Dish, ProductDish, Category, Product } = require("../models/index");
 class Controller {
   static async getDish(req, res, next) {
     try {
-      const data = await Dish.findAll({
-        include: Category,
-      });
+      let options = { include: Category };
+      const { CategoryId } = req.query;
+
+      if (CategoryId) {
+        options = {
+          include: Category,
+          where: {
+            CategoryId,
+          },
+        };
+      }
+
+      const data = await Dish.findAll(options);
       res.status(200).json(data);
     } catch (error) {
       console.log(error);
     }
+  }
+
+  static async getDishByCategory(req, res, next) {
+    try {
+      const { CategoryId } = req.query;
+      const data = await Dish.findAll({ where: { CategoryId } });
+      res.status(200).json(data);
+    } catch (error) {}
   }
 
   static async getDishDetail(req, res, next) {
